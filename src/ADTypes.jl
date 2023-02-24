ab32cd470fc368d4875f6773d6e12afdd0f60b88
Vaibhav Dixit <vaibhavyashdixit@gmail.com>
2023-02-24 08:13:54 +0530

    Initial commit

--- src/ADTypes.jl
+++ src/ADTypes.jl
@@ -1,5 +1,35 @@
 module ADTypes
 
-# Write your package code here.
+"""
+Base type for AD choices.
+"""
+abstract type AbstractADType end
+
+struct AutoFiniteDiff{T1,T2,T3} <: AbstractADType
+    fdtype::T1
+    fdjtype::T2
+    fdhtype::T3
+end
+
+function AutoFiniteDiff(; fdtype=Val(:forward), fdjtype=fdtype,
+    fdhtype=Val(:hcentral))
+    AutoFiniteDiff(fdtype, fdjtype, fdhtype)
+end
+
+struct AutoForwardDiff{chunksize} <: AbstractADType end
+
+function AutoForwardDiff(chunksize=nothing)
+    AutoForwardDiff{chunksize}()
+end
+
+struct AutoReverseDiff <: AbstractADType
+    compile::Bool
+end
+
+AutoReverseDiff(; compile=false) = AutoReverseDiff(compile)
+
+struct AutoZygote <: AbstractADType end
+
+struct AutoTracker <: AbstractADType end
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -2,5 +2,23 @@ using ADTypes
 using Test
 
 @testset "ADTypes.jl" begin
-    # Write your tests here.
+    adtype = AutoFiniteDiff()
+    @test adtype isa AbstractADType
+    @test adtype isa AutoFiniteDiff
+
+    adtype = AutoForwardDiff()
+    @test adtype isa AbstractADType
+    @test adtype isa AutoForwardDiff
+
+    adtype = AutoReverseDiff()
+    @test adtype isa AbstractADType
+    @test adtype isa AutoReverseDiff
+
+    adtype = AutoZygote()
+    @test adtype isa AbstractADType
+    @test adtype isa AutoZygote
+
+    adtype = AutoTracker()
+    @test adtype isa AbstractADType
+    @test adtype isa AutoTracker
 end
