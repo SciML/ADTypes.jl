ed687ea9cd854a6621ee24488a1a517dcdaee17b
Vaibhav Dixit <vaibhavyashdixit@gmail.com>
2023-02-24 08:31:00 +0530

    Export types

--- src/ADTypes.jl
+++ src/ADTypes.jl
@@ -5,20 +5,20 @@ Base type for AD choices.
 """
 abstract type AbstractADType end
 
-struct AutoFiniteDiff{T1,T2,T3} <: AbstractADType
+struct AutoFiniteDiff{T1, T2, T3} <: AbstractADType
     fdtype::T1
     fdjtype::T2
     fdhtype::T3
 end
 
-function AutoFiniteDiff(; fdtype=Val(:forward), fdjtype=fdtype,
-    fdhtype=Val(:hcentral))
+function AutoFiniteDiff(; fdtype = Val(:forward), fdjtype = fdtype,
+                        fdhtype = Val(:hcentral))
     AutoFiniteDiff(fdtype, fdjtype, fdhtype)
 end
 
 struct AutoForwardDiff{chunksize} <: AbstractADType end
 
-function AutoForwardDiff(chunksize=nothing)
+function AutoForwardDiff(chunksize = nothing)
     AutoForwardDiff{chunksize}()
 end
 
@@ -26,10 +26,11 @@ struct AutoReverseDiff <: AbstractADType
     compile::Bool
 end
 
-AutoReverseDiff(; compile=false) = AutoReverseDiff(compile)
+AutoReverseDiff(; compile = false) = AutoReverseDiff(compile)
 
 struct AutoZygote <: AbstractADType end
 
 struct AutoTracker <: AbstractADType end
 
+export AutoFiniteDiff, AutoForwardDiff, AutoReverseDiff, AutoZygote, AutoTracker
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -3,22 +3,22 @@ using Test
 
 @testset "ADTypes.jl" begin
     adtype = AutoFiniteDiff()
-    @test adtype isa AbstractADType
+    @test adtype isa ADTypes.AbstractADType
     @test adtype isa AutoFiniteDiff
 
     adtype = AutoForwardDiff()
-    @test adtype isa AbstractADType
+    @test adtype isa ADTypes.AbstractADType
     @test adtype isa AutoForwardDiff
 
     adtype = AutoReverseDiff()
-    @test adtype isa AbstractADType
+    @test adtype isa ADTypes.AbstractADType
     @test adtype isa AutoReverseDiff
 
     adtype = AutoZygote()
-    @test adtype isa AbstractADType
+    @test adtype isa ADTypes.AbstractADType
     @test adtype isa AutoZygote
 
     adtype = AutoTracker()
-    @test adtype isa AbstractADType
+    @test adtype isa ADTypes.AbstractADType
     @test adtype isa AutoTracker
 end
