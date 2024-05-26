85198de2752290b1e782aa02d5b11319490ec4e8
ArnoStrouwen <arno.strouwen@telenet.be>
2024-05-26 12:45:05 +0200

    typos CI

--- src/sparse.jl
+++ src/sparse.jl
@@ -88,7 +88,7 @@ function row_coloring end
 """
     symmetric_coloring(M::AbstractMatrix, ca::ColoringAlgorithm)::AbstractVector{<:Integer}
 
-Use algorithm `ca` to construct a symetrically structurally orthogonal partition of the columns (or rows) of the symmetric matrix `M`.
+Use algorithm `ca` to construct a symmetrically structurally orthogonal partition of the columns (or rows) of the symmetric matrix `M`.
 
 The result is a coloring vector `c` of length `size(M, 1) == size(M, 2)` such that for every non-zero coefficient `M[i, j]`, at least one of the following conditions holds:
 
