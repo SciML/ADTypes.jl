e00e0876f67695356ec1a45ab82e7b2ebf8d1f65
Vaibhav Kumar Dixit <vaibhavyashdixit@gmail.com>
2024-06-06 20:08:13 -0400

    Update src/legacy.jl
    Co-authored-by: Christopher Rackauckas <accounts@chrisrackauckas.com>

--- src/legacy.jl
+++ src/legacy.jl
@@ -12,7 +12,7 @@
 @deprecate AutoSparseZygote() AutoSparse(AutoZygote())
 
 function AutoModelingToolkit(sparse = false, cons_sparse = false; kwargs...)
-    @warn "AutoModelingToolkit(args...) is deprecated, use AutoSymbolics() or AutoSparse(AutoSymbolics()) instead."
+    @warn "AutoModelingToolkit(args...) is deprecated, use AutoSymbolics() or AutoSparse(AutoSymbolics()) instead."  maxlog=1
     if sparse || cons_sparse
         return AutoSparse(AutoSymbolics())
     else
