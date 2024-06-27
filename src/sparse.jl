59ceff38b010a8793ee2be4164c9941b73f94057
Guillaume Dalle <22795598+gdalle@users.noreply.github.com>
2024-06-27 06:52:01 +0200

    Fix pretty printing and ReverseDiff constructor (#67)

--- src/sparse.jl
+++ src/sparse.jl
@@ -155,15 +155,15 @@ function AutoSparse(
 end
 
 function Base.show(io::IO, backend::AutoSparse)
-    s = "AutoSparse(dense_ad=$(repr(backend.dense_ad, context=io)), "
+    print(io, AutoSparse, "(dense_ad=", repr(backend.dense_ad, context = io))
     if backend.sparsity_detector != NoSparsityDetector()
-        s *= "sparsity_detector=$(repr(backend.sparsity_detector, context=io)), "
+        print(io, ", sparsity_detector=", repr(backend.sparsity_detector, context = io))
     end
     if backend.coloring_algorithm != NoColoringAlgorithm()
-        s *= "coloring_algorithm=$(repr(backend.coloring_algorithm, context=io))), "
+        print(
+            io, ", coloring_algorithm=", repr(backend.coloring_algorithm, context = io))
     end
-    s = s[1:(end - 2)] * ")"
-    print(io, s)
+    print(io, ")")
 end
 
 """
--- test/misc.jl
+++ test/misc.jl
@@ -7,6 +7,7 @@ end
 @testset "Printing" begin
     for ad in every_ad_with_options()
         @test startswith(string(ad), "Auto")
+        @test contains(string(ad), "(")
         @test endswith(string(ad), ")")
     end
 
@@ -18,4 +19,41 @@ end
     )
     @test contains(string(sparse_backend1), string(AutoForwardDiff()))
     @test length(string(sparse_backend1)) < length(string(sparse_backend2))
+end
+
+import ADTypes
+
+struct FakeSparsityDetector <: ADTypes.AbstractSparsityDetector end
+struct FakeColoringAlgorithm <: ADTypes.AbstractColoringAlgorithm end
+
+for backend in [
+    # dense
+    ADTypes.AutoChainRules(; ruleconfig = :rc),
+    ADTypes.AutoDiffractor(),
+    ADTypes.AutoEnzyme(),
+    ADTypes.AutoEnzyme(mode = :forward),
+    ADTypes.AutoFastDifferentiation(),
+    ADTypes.AutoFiniteDiff(),
+    ADTypes.AutoFiniteDiff(fdtype = :fd, fdjtype = :fdj, fdhtype = :fdh),
+    ADTypes.AutoFiniteDifferences(; fdm = :fdm),
+    ADTypes.AutoForwardDiff(),
+    ADTypes.AutoForwardDiff(chunksize = 3, tag = :tag),
+    ADTypes.AutoPolyesterForwardDiff(),
+    ADTypes.AutoPolyesterForwardDiff(chunksize = 3, tag = :tag),
+    ADTypes.AutoReverseDiff(),
+    ADTypes.AutoReverseDiff(compile = true),
+    ADTypes.AutoSymbolics(),
+    ADTypes.AutoTapir(),
+    ADTypes.AutoTapir(safe_mode = false),
+    ADTypes.AutoTracker(),
+    ADTypes.AutoZygote(),
+    # sparse
+    ADTypes.AutoSparse(ADTypes.AutoForwardDiff()),
+    ADTypes.AutoSparse(
+        ADTypes.AutoForwardDiff();
+        sparsity_detector = FakeSparsityDetector(),
+        coloring_algorithm = FakeColoringAlgorithm()
+    )
+]
+    println(backend)
 end
