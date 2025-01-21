716acbef0615c9e309fbb543a16a9e69af909aa2
1-Bart-1 <bart@vandelint.net>
2025-01-21 13:33:28 +0100

    add relstep and absstep documentation

--- src/dense.jl
+++ src/dense.jl
@@ -106,13 +106,15 @@ Defined by [ADTypes.jl](https://github.com/SciML/ADTypes.jl).
 
 # Constructors
 
-    AutoFiniteDiff(; fdtype=Val(:forward), fdjtype=fdtype, fdhtype=Val(:hcentral))
+    AutoFiniteDiff(; fdtype=Val(:forward), fdjtype=fdtype, fdhtype=Val(:hcentral), relstep=nothing, absstep=nothing)
 
 # Fields
 
   - `fdtype::T1`: finite difference type
   - `fdjtype::T2`: finite difference type for the Jacobian
   - `fdhtype::T3`: finite difference type for the Hessian
+  - `relstep: relative finite difference step size`
+  - `absstep: absolute finite difference step size`
 """
 Base.@kwdef struct AutoFiniteDiff{T1, T2, T3} <: AbstractADType
     fdtype::T1 = Val(:forward)
